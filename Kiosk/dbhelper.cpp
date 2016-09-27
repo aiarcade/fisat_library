c1ad0de48766f34ebe99cc25781dadaf7d406b39
Mahesh chandrasekharan <mahesh.fisat@gmail.com>
2016-09-27 07:40:39 +0530

    gl enabled

---
+++ Kiosk/dbhelper.cpp
@@ -0,0 +1,6 @@
+#include "dbhelper.h"
+
+DbHelper::DbHelper()
+{
+
+}
---
+++ Kiosk/dbhelper.h
@@ -0,0 +1,11 @@
+#ifndef DBHELPER_H
+#define DBHELPER_H
+
+
+class DbHelper
+{
+public:
+    DbHelper();
+};
+
+#endif // DBHELPER_H
