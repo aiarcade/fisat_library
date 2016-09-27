aa652349a366eb49397400ae6fc99feef544580f
Mahesh chandrasekharan <mahesh.fisat@gmail.com>
2016-09-27 12:14:47 +0530

    display software using qt added

--- Kiosk/glwidget.cpp
+++ Kiosk/glwidget.cpp
@@ -274,7 +274,7 @@
                         quoteImage);
 
      painter->drawText((this->width()/5)+5, (this->height()/5)+600,
-                        400, 75,
+                        800, 200,
                        Qt::AlignCenter | Qt::TextWordWrap ,_quote);
 
 
